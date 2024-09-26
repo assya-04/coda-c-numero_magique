20e73c2569239606424b3fcff1d1eef3a5731263
assya-04 <assiatou.drame@coda-student.school>
2024-09-26 15:50:28 +0200

    etape 2: ajout des variables

--- main.c
+++ main.c
@@ -4,6 +4,36 @@
 
 int main ()
 {
+	srand(time(NULL));
+	int random_number=rand() % 100 + 1;
+	int num;
 
+
+	//printf("random_number :%d\n", random_number);
+
+
+	while(random_number!=num)
+	{
+// Choix du nombre de l'utilisateur
+		printf("Choisissez un nombre entre 0 et 100\n");
+		scanf("%d",&num);
+
+		if(random_number==num)
+		{
+			printf("Gagné !\n");
+		}
+		else if(random_number>num)
+		{
+			printf("C'est plus ! \n");
+		}
+		else 
+		{
+			printf("C'est moins !\n");
+		}
+	}
+
+	
+	
+	
 	exit(0);
 }
